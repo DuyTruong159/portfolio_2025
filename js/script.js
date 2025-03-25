afe83cc8e0b7d96b1b16b1e9ab6c605ad7242f4f
DuyTruong159 <truongnhox159@gmail.com>
2025-03-25 02:43:26 -0400

    animation decode text

--- js/script.js
+++ js/script.js
@@ -1,5 +1,8 @@
 const loadingText = document.querySelector('#ring > div:last-child');
 const loadingProgress = document.querySelector('#progress');
+const decodeText = document.querySelector('#home h2');
+const charDuration = 1 / 20;
+const timeoutDuration = 0.75;
 let percentage = 1;
 
 document.querySelector('#mobile i').addEventListener('click', (e) => {
@@ -11,6 +14,55 @@ document.addEventListener('click', (event) => {
         document.querySelector('#mobile menu').classList.remove('show');
     }
 });
+
+function randomChar() {
+    const str = 'abcdefghijklmnopqrstuvwxyz!@#$%^&*()_+';
+    const c = str[Math.floor(Math.random() * str.length)];
+    return Math.random() > 0.5 ? c : c.toUpperCase();
+}
+
+function easeIn(x) {
+    return x === 0 ? 0 : Math.pow(2, 10 * x - 10);
+}
+
+function animate(e) {
+    let elem = e.target;
+    let text = elem.textContent;
+
+    let newText = [];
+
+    for (let i = 0; i < text.length; i++) {
+        newText.push(randomChar());
+    }
+
+    elem.textContent = newText.join('');
+
+    let i = 0;
+    let int = setInterval(() => {
+        if (i >= text.length) {
+            clearInterval(int);
+        }
+
+        let t = [];
+        for (let j = 0; j <= i; j++) {
+            t.push(text[j]);
+        }
+        for (let j = i + 1; j < newText.length; j++) {
+            t.push(randomChar());
+        }
+        let p  = easeIn(i / text.length);
+        let tim = setTimeout(() => {
+            if (p == 1) {
+                clearTimeout(tim);
+            }
+            let upText = t.join('');
+            elem.textContent = upText;
+        }, p * timeoutDuration);
+        i++;
+    }, charDuration * 1000);
+}
+
+decodeText.addEventListener('mouseenter', animate);
 
 function updateLoading() {
     if (percentage <= 100) {
